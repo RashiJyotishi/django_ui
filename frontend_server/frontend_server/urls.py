5b7de9393d2722f39e2e898b40dd1aa49852348e
RashiJyotishi <jyotishirashi1610@gmail.com>
2026-01-31 15:20:24 +0530

    home page and navbar added

--- frontend_server/frontend_server/urls.py
+++ frontend_server/frontend_server/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path('admin/', admin.site.urls),
     path('login/', views.login_page, name='login'),
     path('signup/', views.signup_page, name='signup'),
