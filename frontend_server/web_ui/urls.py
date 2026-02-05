4f411c329d34c72efe2ab5e7b721f26f4b47659c
RashiJyotishi <jyotishirashi1610@gmail.com>
2026-02-05 22:01:28 +0530

    refactor: clean django routing

--- frontend_server/web_ui/urls.py
+++ frontend_server/web_ui/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("signup/", views.signup_page, name="signup"),
     path("dashboard/", views.dashboard_page, name="dashboard"),
     path("logout/", views.logout_user, name="logout"),
+    path("create-group/", views.create_group, name="create_group"),
+    path("join-group/", views.join_group, name="join_group"),
+    path("add-expense/<int:group_id>/", views.add_expense, name="add_expense"),
+    path("groups/<int:group_id>/simplify/", views.simplify_group, name="simplify"),
+
 ]
